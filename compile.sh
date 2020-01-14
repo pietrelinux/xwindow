07dbd4278ba0f7050a5268fd0a1e3cff488f3bc4
pietre.linux <pietre.linux@gmail.com>
2020-01-14 15:49:26 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+sudo apt-get install asciidoc autoconf automake autopoint autotools-dev  bison docbook-utils doxygen flex fop git-core  gperf intltool  libdrm-dev libfontconfig1-dev libfreetype6-dev libgbm-dev libglib2.0-dev libmtdev-dev libncurses5-dev texlive-formats-extra libpng-dev libssl-dev libtool libudev-dev llvm m4  netpbm psutils python-mako systemtap-sdt-dev w3m xmlto  zlib1g-dev
+
 mkdir $HOME/src
 mkdir $HOME/build
 cd $HOME/src
